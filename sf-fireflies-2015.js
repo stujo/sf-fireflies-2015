3b2711b7f127ad45039fef8d85ebb3e58259b85a
Stujo <stujo@users.noreply.github.com>
2015-08-12 09:36:32 -0700

    Update sf-fireflies-2015.js

--- sf-fireflies-2015.js
+++ sf-fireflies-2015.js
@@ -50,8 +50,11 @@ function draw(words) {
     .append("div")
     .classed("svg-container", true) //container class to make it responsive
     .append("svg")
-      .attr("width", layout.size()[0])
-      .attr("height", layout.size()[1])
+      //responsive SVG needs these 2 attributes and no width and height attr
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 600 400")
+      //class to make it responsive
+      .classed("svg-content-responsive", true);     
     .append("g")
       .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
     .selectAll("text")
